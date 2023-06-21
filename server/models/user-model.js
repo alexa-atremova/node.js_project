cd6dd0d2527aa99fd77232ca83a9f45bd79cb601
alexa-atremova <77694102+alexa-atremova@users.noreply.github.com>
2023-06-21 19:41:11 +0300

    model/router/controller added

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -3,8 +3,8 @@ const { Schema, model } = require("mongoose");
 const UserSchema = new Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  isAtivated: { type: Boolean, default: false },
-  activationLink: { type: String, required: true },
+  isActivated: { type: Boolean, default: false },
+  activationLink: { type: String },
 });
 
 module.exports = model("User", UserSchema);
